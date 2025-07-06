1e1093c89a8616277c9e0ff62a500bde4c826be8
Nisanureser <Nisa2415@hotmail.com>
2025-07-06 12:45:14 +0300

    Uploaded basic calculator app built with C#

---
+++ Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ScreenLabel = new System.Windows.Forms.Label();
+            this.rakam0button = new System.Windows.Forms.Button();
+            this.rakam1button = new System.Windows.Forms.Button();
+            this.rakam2button = new System.Windows.Forms.Button();
+            this.rakam3button = new System.Windows.Forms.Button();
+            this.rakam4button = new System.Windows.Forms.Button();
+            this.rakam5button = new System.Windows.Forms.Button();
+            this.rakam6button = new System.Windows.Forms.Button();
+            this.rakam7button = new System.Windows.Forms.Button();
+            this.rakam8button = new System.Windows.Forms.Button();
+            this.rakam9button = new System.Windows.Forms.Button();
+            this.artıButton = new System.Windows.Forms.Button();
+            this.eksiButton = new System.Windows.Forms.Button();
+            this.çarpmaButton = new System.Windows.Forms.Button();
+            this.bölmeButton = new System.Windows.Forms.Button();
+            this.eşittirButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.btnDot = new System.Windows.Forms.Button();
+            this.btnMod = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ScreenLabel
+            // 
+            this.ScreenLabel.BackColor = System.Drawing.Color.White;
+            this.ScreenLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ScreenLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F);
+            this.ScreenLabel.Location = new System.Drawing.Point(30, 20);
+            this.ScreenLabel.Name = "ScreenLabel";
+            this.ScreenLabel.Size = new System.Drawing.Size(320, 70);
+            this.ScreenLabel.TabIndex = 0;
+            this.ScreenLabel.Text = "0";
+            this.ScreenLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // rakam0button
+            // 
+            this.rakam0button.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.rakam0button.Location = new System.Drawing.Point(110, 360);
+            this.rakam0button.Name = "rakam0button";
+            this.rakam0button.Size = new System.Drawing.Size(70, 70);
+            this.rakam0button.TabIndex = 1;
+            this.rakam0button.Text = "0";
+            this.rakam0button.Click += new System.EventHandler(this.NumberButton_Click);
+            // 
+            // rakam1button
+            // 
+            this.rakam1button.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.rakam1button.Location = new System.Drawing.Point(30, 280);
+            this.rakam1button.Name = "rakam1button";
+            this.rakam1button.Size = new System.Drawing.Size(70, 70);
+            this.rakam1button.TabIndex = 2;
+            this.rakam1button.Text = "1";
+            this.rakam1button.Click += new System.EventHandler(this.NumberButton_Click);
+            // 
+            // rakam2button
+            // 
+            this.rakam2button.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.rakam2button.Location = new System.Drawing.Point(110, 280);
+            this.rakam2button.Name = "rakam2button";
+            this.rakam2button.Size = new System.Drawing.Size(70, 70);
+            this.rakam2button.TabIndex = 3;
+            this.rakam2button.Text = "2";
+            this.rakam2button.Click += new System.EventHandler(this.NumberButton_Click);
+            // 
+            // rakam3button
+            // 
+            this.rakam3button.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.rakam3button.Location = new System.Drawing.Point(190, 280);
+            this.rakam3button.Name = "rakam3button";
+            this.rakam3button.Size = new System.Drawing.Size(70, 70);
+            this.rakam3button.TabIndex = 4;
+            this.rakam3button.Text = "3";
+            this.rakam3button.Click += new System.EventHandler(this.NumberButton_Click);
+            // 
+            // rakam4button
+            // 
+            this.rakam4button.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.rakam4button.Location = new System.Drawing.Point(30, 200);
+            this.rakam4button.Name = "rakam4button";
+            this.rakam4button.Size = new System.Drawing.Size(70, 70);
+            this.rakam4button.TabIndex = 5;
+            this.rakam4button.Text = "4";
+            this.rakam4button.Click += new System.EventHandler(this.NumberButton_Click);
+            // 
+            // rakam5button
+            // 
+            this.rakam5button.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.rakam5button.Location = new System.Drawing.Point(110, 200);
+            this.rakam5button.Name = "rakam5button";
+            this.rakam5button.Size = new System.Drawing.Size(70, 70);
+            this.rakam5button.TabIndex = 6;
+            this.rakam5button.Text = "5";
+            this.rakam5button.Click += new System.EventHandler(this.NumberButton_Click);
+            // 
+            // rakam6button
+            // 
+            this.rakam6button.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.rakam6button.Location = new System.Drawing.Point(190, 200);
+            this.rakam6button.Name = "rakam6button";
+            this.rakam6button.Size = new System.Drawing.Size(70, 70);
+            this.rakam6button.TabIndex = 7;
+            this.rakam6button.Text = "6";
+            this.rakam6button.Click += new System.EventHandler(this.NumberButton_Click);
+            // 
+            // rakam7button
+            // 
+            this.rakam7button.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.rakam7button.Location = new System.Drawing.Point(30, 120);
+            this.rakam7button.Name = "rakam7button";
+            this.rakam7button.Size = new System.Drawing.Size(70, 70);
+            this.rakam7button.TabIndex = 8;
+            this.rakam7button.Text = "7";
+            this.rakam7button.Click += new System.EventHandler(this.NumberButton_Click);
+            // 
+            // rakam8button
+            // 
+            this.rakam8button.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.rakam8button.Location = new System.Drawing.Point(110, 120);
+            this.rakam8button.Name = "rakam8button";
+            this.rakam8button.Size = new System.Drawing.Size(70, 70);
+            this.rakam8button.TabIndex = 9;
+            this.rakam8button.Text = "8";
+            this.rakam8button.Click += new System.EventHandler(this.NumberButton_Click);
+            // 
+            // rakam9button
+            // 
+            this.rakam9button.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.rakam9button.Location = new System.Drawing.Point(190, 120);
+            this.rakam9button.Name = "rakam9button";
+            this.rakam9button.Size = new System.Drawing.Size(70, 70);
+            this.rakam9button.TabIndex = 10;
+            this.rakam9button.Text = "9";
+            this.rakam9button.Click += new System.EventHandler(this.NumberButton_Click);
+            // 
+            // artıButton
+            // 
+            this.artıButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.artıButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.artıButton.Location = new System.Drawing.Point(270, 120);
+            this.artıButton.Name = "artıButton";
+            this.artıButton.Size = new System.Drawing.Size(80, 70);
+            this.artıButton.TabIndex = 11;
+            this.artıButton.Text = "+";
+            this.artıButton.UseVisualStyleBackColor = false;
+            this.artıButton.Click += new System.EventHandler(this.OperatorButton_Click);
+            // 
+            // eksiButton
+            // 
+            this.eksiButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.eksiButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.eksiButton.Location = new System.Drawing.Point(270, 200);
+            this.eksiButton.Name = "eksiButton";
+            this.eksiButton.Size = new System.Drawing.Size(80, 70);
+            this.eksiButton.TabIndex = 12;
+            this.eksiButton.Text = "-";
+            this.eksiButton.UseVisualStyleBackColor = false;
+            this.eksiButton.Click += new System.EventHandler(this.OperatorButton_Click);
+            // 
+            // çarpmaButton
+            // 
+            this.çarpmaButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.çarpmaButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.çarpmaButton.Location = new System.Drawing.Point(270, 280);
+            this.çarpmaButton.Name = "çarpmaButton";
+            this.çarpmaButton.Size = new System.Drawing.Size(80, 70);
+            this.çarpmaButton.TabIndex = 13;
+            this.çarpmaButton.Text = "*";
+            this.çarpmaButton.UseVisualStyleBackColor = false;
+            this.çarpmaButton.Click += new System.EventHandler(this.OperatorButton_Click);
+            // 
+            // bölmeButton
+            // 
+            this.bölmeButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.bölmeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.bölmeButton.Location = new System.Drawing.Point(270, 360);
+            this.bölmeButton.Name = "bölmeButton";
+            this.bölmeButton.Size = new System.Drawing.Size(80, 70);
+            this.bölmeButton.TabIndex = 14;
+            this.bölmeButton.Text = "/";
+            this.bölmeButton.UseVisualStyleBackColor = false;
+            this.bölmeButton.Click += new System.EventHandler(this.OperatorButton_Click);
+            // 
+            // eşittirButton
+            // 
+            this.eşittirButton.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.eşittirButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.eşittirButton.Location = new System.Drawing.Point(190, 360);
+            this.eşittirButton.Name = "eşittirButton";
+            this.eşittirButton.Size = new System.Drawing.Size(70, 70);
+            this.eşittirButton.TabIndex = 15;
+            this.eşittirButton.Text = "=";
+            this.eşittirButton.UseVisualStyleBackColor = false;
+            this.eşittirButton.Click += new System.EventHandler(this.BtnEquals_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.clearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.clearButton.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.clearButton.Location = new System.Drawing.Point(30, 440);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(320, 50);
+            this.clearButton.TabIndex = 16;
+            this.clearButton.Text = "C";
+            this.clearButton.UseVisualStyleBackColor = false;
+            this.clearButton.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // btnDot
+            // 
+            this.btnDot.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnDot.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.btnDot.Location = new System.Drawing.Point(30, 360);
+            this.btnDot.Name = "btnDot";
+            this.btnDot.Size = new System.Drawing.Size(70, 70);
+            this.btnDot.TabIndex = 17;
+            this.btnDot.Text = ",";
+            this.btnDot.UseVisualStyleBackColor = false;
+            this.btnDot.Click += new System.EventHandler(this.BtnDot_Click);
+            // 
+            // btnMod
+            // 
+            this.btnMod.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.btnMod.Location = new System.Drawing.Point(110, 440);
+            this.btnMod.Name = "btnMod";
+            this.btnMod.Size = new System.Drawing.Size(70, 50);
+            this.btnMod.TabIndex = 18;
+            this.btnMod.Text = "%";
+            this.btnMod.Click += new System.EventHandler(this.OperatorButton_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(400, 510);
+            this.Controls.Add(this.ScreenLabel);
+            this.Controls.Add(this.rakam0button);
+            this.Controls.Add(this.rakam1button);
+            this.Controls.Add(this.rakam2button);
+            this.Controls.Add(this.rakam3button);
+            this.Controls.Add(this.rakam4button);
+            this.Controls.Add(this.rakam5button);
+            this.Controls.Add(this.rakam6button);
+            this.Controls.Add(this.rakam7button);
+            this.Controls.Add(this.rakam8button);
+            this.Controls.Add(this.rakam9button);
+            this.Controls.Add(this.artıButton);
+            this.Controls.Add(this.eksiButton);
+            this.Controls.Add(this.çarpmaButton);
+            this.Controls.Add(this.bölmeButton);
+            this.Controls.Add(this.eşittirButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.btnDot);
+            this.Controls.Add(this.btnMod);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Hesap Makinesi";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ScreenLabel;
+        private System.Windows.Forms.Button rakam0button;
+        private System.Windows.Forms.Button rakam1button;
+        private System.Windows.Forms.Button rakam2button;
+        private System.Windows.Forms.Button rakam3button;
+        private System.Windows.Forms.Button rakam4button;
+        private System.Windows.Forms.Button rakam5button;
+        private System.Windows.Forms.Button rakam6button;
+        private System.Windows.Forms.Button rakam7button;
+        private System.Windows.Forms.Button rakam8button;
+        private System.Windows.Forms.Button rakam9button;
+        private System.Windows.Forms.Button artıButton;
+        private System.Windows.Forms.Button eksiButton;
+        private System.Windows.Forms.Button çarpmaButton;
+        private System.Windows.Forms.Button bölmeButton;
+        private System.Windows.Forms.Button eşittirButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button btnDot;
+        private System.Windows.Forms.Button btnMod;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        private double _firstNumber = 0;
+        private string _operation = "";
+        private bool _isNewEntry = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void NumberButton_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            if (_isNewEntry || ScreenLabel.Text == "0")
+            {
+                ScreenLabel.Text = btn.Text;
+                _isNewEntry = false;
+            }
+            else
+            {
+                ScreenLabel.Text += btn.Text;
+            }
+        }
+
+        private void OperatorButton_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            _firstNumber = double.Parse(ScreenLabel.Text);
+            _operation = btn.Text;
+            _isNewEntry = true;
+        }
+
+        private void BtnEquals_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double secondNumber = double.Parse(ScreenLabel.Text);
+                double result = 0;
+
+                switch (_operation)
+                {
+                    case "+":
+                        result = _firstNumber + secondNumber;
+                        break;
+                    case "-":
+                        result = _firstNumber - secondNumber;
+                        break;
+                    case "*":
+                        result = _firstNumber * secondNumber;
+                        break;
+                    case "/":
+                        if (secondNumber == 0)
+                        {
+                            MessageBox.Show("Sıfıra bölme hatası!");
+                            return;
+                        }
+                        result = _firstNumber / secondNumber;
+                        break;
+                    case "%":
+                        result = _firstNumber % secondNumber;
+                        break;
+                    default:
+                        MessageBox.Show("Lütfen işlem seçiniz!");
+                        return;
+                }
+
+                ScreenLabel.Text = result.ToString();
+                _isNewEntry = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata: " + ex.Message);
+            }
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            ScreenLabel.Text = "0";
+            _firstNumber = 0;
+            _operation = "";
+            _isNewEntry = true;
+        }
+
+        private void BtnDot_Click(object sender, EventArgs e)
+        {
+            if (_isNewEntry)
+            {
+                ScreenLabel.Text = "0,";
+                _isNewEntry = false;
+            }
+            else if (!ScreenLabel.Text.Contains(","))
+            {
+                ScreenLabel.Text += ",";
+            }
+        }
+    }
+}
